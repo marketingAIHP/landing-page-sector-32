0469be02111f4ffac01c6e608e2fe354af0e7303
marketingAIHP <marketing@aihp.in>
2026-03-17 18:11:14 +0530

    GA4

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 
 const googleAnalyticsId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
 const metaPixelId = process.env.NEXT_PUBLIC_META_PIXEL_ID;
+const linkedInPartnerId = process.env.NEXT_PUBLIC_LINKEDIN_PARTNER_ID;
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://sector-32.aihp.in"),
@@ -66,6 +67,41 @@ export default function RootLayout({
                 gtag('config', '${googleAnalyticsId}');
               `}
             </Script>
+          </>
+        ) : null}
+        {linkedInPartnerId ? (
+          <>
+            <Script id="linkedin-insight" strategy="afterInteractive">
+              {`
+                window._linkedin_partner_id = "${linkedInPartnerId}";
+                window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+                window._linkedin_data_partner_ids.push(window._linkedin_partner_id);
+                (function(l) {
+                  if (!l) {
+                    window.lintrk = function(a, b) {
+                      window.lintrk.q.push([a, b]);
+                    };
+                    window.lintrk.q = [];
+                  }
+                  var s = document.getElementsByTagName("script")[0];
+                  var b = document.createElement("script");
+                  b.type = "text/javascript";
+                  b.async = true;
+                  b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+                  s.parentNode.insertBefore(b, s);
+                })(window.lintrk);
+              `}
+            </Script>
+            <noscript>
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                height="1"
+                width="1"
+                style={{ display: "none" }}
+                src={`https://px.ads.linkedin.com/collect/?pid=${linkedInPartnerId}&fmt=gif`}
+                alt=""
+              />
+            </noscript>
           </>
         ) : null}
       </body>
